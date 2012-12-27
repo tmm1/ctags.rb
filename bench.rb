d26cd6522d58b4ec94e85a2af68149e761b6a6e1
Aman Gupta <aman@tmm1.net>
2012-12-27 00:34:24 -0800

    more benchmarks

--- bench.rb
+++ bench.rb
@@ -3,12 +3,26 @@ require 'rubygems'
 $:.unshift File.expand_path('../lib', __FILE__)
 require 'ctags'
 
-Benchmark.bmbm(20) do |b|
-  b.report('tags for file') do
-    Ctags.tags_for_file(__FILE__)
-  end
+Benchmark.bmbm(30) do |b|
+  [
+    ['small', __FILE__],
+    ['medium', Gem.find_files('mkmf.rb')[0]],
+    ['big', Gem.find_files('tk.rb')[0]]
+  ].each do |type, file|
+    b.report("tags for #{type} file") do
+      Ctags.tags_for_file(file)
+    end
 
-  b.report('tags for file x1000') do
-    1000.times{ Ctags.tags_for_file(__FILE__) }
+    b.report("tags for #{type} file x500") do
+      500.times{ Ctags.tags_for_file(file) }
+    end
+
+    b.report("tags for #{type} blob") do
+      Ctags.tags_for_file(file, File.read(file))
+    end
+
+    b.report("tags for #{type} blob x500") do
+      500.times{ Ctags.tags_for_file(file, File.read(file)) }
+    end
   end
 end
