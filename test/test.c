6c826ff2256f6984dc214137412a8dd65696102b
Aman Gupta <aman@tmm1.net>
2016-11-29 15:10:28 -0800

    add tests for universal-ctags parsing

---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void hello_world() {
+  printf("hello world\n");
+}
+
+int main() {
+  hello_world();
+  return 0;
+}
--- test/test_ctags.rb
+++ test/test_ctags.rb
@@ -3,7 +3,7 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require 'ctags'
 
 class CtagsTest < MiniTest::Test
-  def test_tags_for_file
+  def test_tags_for_ruby_file
     tags = Ctags.tags_for_file(__FILE__)
     tag = tags.find{ |t| t[:name] == __method__.to_s }
 
@@ -13,7 +13,7 @@ class CtagsTest < MiniTest::Test
     assert_equal 'method',    tag[:kind]
   end
 
-  def test_tags_for_file_with_code
+  def test_tags_for_ruby_file_with_code
     tags = Ctags.tags_for_file('file.rb', File.read(__FILE__))
     tag = tags.find{ |t| t[:name] == __method__.to_s }
 
@@ -23,9 +23,27 @@ class CtagsTest < MiniTest::Test
     assert_equal 'method',    tag[:kind]
   end
 
-  def test_invalid_file
+  def test_invalid_ruby_file
     assert_raises Ctags::Error do
       Ctags.tags_for_file('invalid.rb')
+    end
+  end
+
+  def test_tags_for_file
+    tags = Ctags.tags_for_file(File.expand_path("../test.c", __FILE__))
+    assert_equal 2, tags.size
+    assert_equal %w[ hello_world main ], tags.map{ |t| t[:name] }
+  end
+
+  def test_tags_for_file_with_code
+    tags = Ctags.tags_for_file("test.c", IO.binread(File.expand_path("../test.c", __FILE__)))
+    assert_equal 2, tags.size
+    assert_equal %w[ hello_world main ], tags.map{ |t| t[:name] }
+  end
+
+  def test_invalid_file
+    assert_raises Ctags::Error do
+      Ctags.tags_for_file('invalid.c')
     end
   end
 end
